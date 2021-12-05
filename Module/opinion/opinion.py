d0bae404ada6291a38a69231fd6b9cf965094a44
noeli <noe@libon.eu>
2021-12-05 19:36:21 +0100

    db par def

--- Module/opinion/opinion.py
+++ Module/opinion/opinion.py
@@ -1,12 +1,21 @@
 #Noé Libon
 #https://www.w3schools.com/python/python_mongodb_getstarted.asp
 
-import requests
+import pymongo
+
 
 class Opinion:
     def __init__(self, is_positif, message=""):
         self.__opinion = is_positif
         self.__message = message
+        self.send_db()
 
-    def send_DB(self):
-        pass
+    def send_db(self):
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["Opinions"]
+        print(client.list_database_names())
+        db_list = client.list_database_names()
+        if "mydatabase" in db_list:
+            print("The database exists.")
+        else:
+            print("Doesn't exists ! ")
