02706c23e896846e163562f1c2ab1ddf47ba494c
noeli <noe@libon.eu>
2021-12-06 01:16:18 +0100

    db par def

--- Module/opinion/opinion.py
+++ Module/opinion/opinion.py
@@ -2,20 +2,33 @@
 #https://www.w3schools.com/python/python_mongodb_getstarted.asp
 
 import pymongo
+from pymongo import MongoClient
 
 
 class Opinion:
-    def __init__(self, is_positif, message=""):
+    def __init__(self, is_positif=5, message="super"):
         self.__opinion = is_positif
         self.__message = message
         self.send_db()
 
     def send_db(self):
-        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        client = MongoClient("mongodb+srv://noeli:user123@pyp.ajkyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
         db = client["Opinions"]
+        collection = db.opinion
+        votes = {
+                "opinion" : self.__opinion,
+                "message" : self.__message
+                }
+        collection.insert_one(votes)
         print(client.list_database_names())
         db_list = client.list_database_names()
-        if "mydatabase" in db_list:
+        if "Opinions" in db_list:
             print("The database exists.")
         else:
             print("Doesn't exists ! ")
+
+
+
+#test
+test1 = Opinion()
+test2 = Opinion(is_positif=2, message="pas ouf")
