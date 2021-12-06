928bf71474f3f4f922b7c6d914521683efffd506
noeli <noe@libon.eu>
2021-12-06 10:32:21 +0100

    adaptation db ephecom (pas encore reussi)

--- Module/opinion/opinion.py
+++ Module/opinion/opinion.py
@@ -12,17 +12,18 @@ class Opinion:
         self.send_db()
 
     def send_db(self):
-        client = MongoClient("mongodb+srv://noeli:user123@pyp.ajkyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-        db = client["Opinions"]
-        collection = db.opinion
-        votes = {
+        #client = MongoClient("mongodb+srv://noeli:user123@pyp.ajkyd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        client = MongoClient("mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE")
+        db = client["ephecom-2TM2"]
+        collection_opinions = db.opinions
+        personal_opinion = {
                 "opinion" : self.__opinion,
                 "message" : self.__message
                 }
-        collection.insert_one(votes)
+        collection_opinions.insert_one(personal_opinion)
         print(client.list_database_names())
         db_list = client.list_database_names()
-        if "Opinions" in db_list:
+        if "ephecom-2TM2" in db_list:
             print("The database exists.")
         else:
             print("Doesn't exists ! ")
