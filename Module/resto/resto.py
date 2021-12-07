762e27bfe52840124ad00796585758cf57a5f943
PatsonEph <c.tiakongouadje@students.ephec.be>
2021-12-07 23:38:55 +0100

    Mise en place d'un interface graphique / Update cine

--- Module/resto/resto.py
+++ Module/resto/resto.py
@@ -1,3 +1,20 @@
+from ..data.config import resto_link
+import requests
+
+
 class Resto:
-    def __init__(self):
-        pass
+
+    def __init__(self, origin='Louvain-la-Neuve'):
+        self.__origin = origin
+        self.__url_origin = resto_link(self.__origin)
+
+    def get_resto(self):
+        response = requests.get(self.__url_origin).json()
+
+        print(response)
+
+        for i in response:
+            print(f"{i['display_name']}\n")
+
+
+Resto('Rixensart')
