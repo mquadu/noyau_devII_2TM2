7ff410549414703886b1e796dabb6cb0935f511c
PatsonEph <c.tiakongouadje@students.ephec.be>
2021-12-23 13:06:03 +0100

    complete test bot

--- test/test_bot.py
+++ test/test_bot.py
@@ -1,30 +1,29 @@
-from unittest import TestCase
+import unittest
 from Module.bot.bot import Bot
 from Module.data.config import COMMAND_LIST, HELP_FILE
 from Module.request.request import Request
 
 
-class TestBot(TestCase):
+class TestBot(unittest.TestCase):
 
     def setUp(self) -> None:
         self.true_command_list = COMMAND_LIST
         self.false_command_list_1 = ["/", "/help", "inutile", "/zarbi"]
         self.message_1 = Request("/help")
         self.message_2 = ["/cine", "namur"]
-        self.message_3 = ["/resto"]  # Dans le cas ou on ne precise pas doit renvoyer celle de louvain-la-neuve
-        self.message_4 = ["/zerjizoiejr", "je ne suis pas une commande"]
 
-    def test_init(self):
+    def test_init(self):    
         pass
 
     def test_get_help(self):
-        # self.assertRaises(FileNotFoundError, Bot(Request("/help"), self.true_command_list).get_help, "")
-        self.assertEqual(Bot(Request("/help"), self.true_command_list).get_help(HELP_FILE), "")
+        self.assertEqual(len(Bot(Request("/help"), self.true_command_list).get_help(HELP_FILE)), 911)
 
     def test_process_request(self):
-        # self.assertEqual(Bot(self.message_1, self.true_command_list).get_help(HELP_FILE), "")
-
         self.assertEqual(len(Bot(Request("/itinerary louvain-la-neuve ottignies"), self.true_command_list).process_request(Request("/itinerary louvain-la-neuve ottignies").get_message(self.true_command_list))), 1201)
         self.assertEqual(len(Bot(Request("/cine namur"), self.true_command_list).process_request(Request("/cine namur").get_message(self.true_command_list))), 169)
         self.assertEqual(len(Bot(Request("/news basket"), self.true_command_list).process_request(Request("/news basket").get_message(self.true_command_list))), 89)
-        self.assertEqual(len(Bot(Request("/cine"), self.true_command_list).process_request(Request("/cine").get_message(self.true_command_list))), 95)
+        self.assertEqual(len(Bot(Request("/resto gembloux"), self.true_command_list).process_request(Request("/resto gembloux").get_message(self.true_command_list))), 79)
+
+
+if __name__ == "__main__":
+    unittest.main()
