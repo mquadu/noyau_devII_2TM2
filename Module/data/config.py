832184b061f432fb89c69c28131ff959a81da7c7
PatsonEph <c.tiakongouadje@students.ephec.be>
2021-12-26 20:09:45 +0100

    fix check_connection error

--- Module/data/config.py
+++ Module/data/config.py
@@ -56,6 +56,6 @@ def check_conection():
     try:
         socket.setdefaulttimeout(3)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
-        return True
-    except socket.error as ex:
         return False
+    except socket.error as ex:
+        return True
