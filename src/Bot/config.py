ef0ec0124f50db6dd7c6da1e7c110171e58b89ba
CardinPatson <74672498+CardinPatson@users.noreply.github.com>
2022-01-23 14:28:06 +0100

    Final config

--- src/Bot/config.py
+++ src/Bot/config.py
@@ -5,6 +5,7 @@ import socket
 
 # RENOMER CETTE VARIABLE AVEC LE NOM DU DOSSIER QUI CONTIENT VOTRE PROJET
 ROOT_DIRECTORY = "noyau_devII_2TM2"
+COMMAND_LIST = ["/help", "/weather", "/itinerary", "/resto", "/cine", "/news", "/opinion"]
 
 # Link open street service
 headers = {'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8', }
@@ -16,19 +17,20 @@ ROOT_DIR = ""
 if sys.platform == "win32":
     if ROOT_DIRECTORY in sys.path[0].split('\\'):
         index_root = sys.path[0].split('\\').index(ROOT_DIRECTORY)
-        ROOT_DIR = "\\".join(sys.path[0].split("\\")[:index_root+1])
+        ROOT_DIR = "\\".join(sys.path[0].split("\\")[:index_root + 1])
         MODULE_DIR = os.path.join(ROOT_DIR, "src")
     else:
         ROOT_DIR = "\\".join(sys.path[-1].split("\\")[:])
         MODULE_DIR = ROOT_DIR
-    
+
     HELP_FILE = os.path.join(MODULE_DIR, "Bot\\help.txt")
-    try :
-      with open(HELP_FILE) as fd:
-        pass
-    except FileNotFoundError:
-      HELP_FILE = os.path.join(sys.path[0].split("\\") , "src\\Bot\\help.txt")
     CERTIFICATE_FILE = os.path.join(MODULE_DIR, "Bot\\db_key.pem")
+    try:
+        with open(HELP_FILE) as fd:
+            pass
+    except FileNotFoundError:
+        HELP_FILE = os.path.join(sys.path[0], "src\\Bot\\help.txt")
+        CERTIFICATE_FILE = os.path.join(MODULE_DIR, "src\\Bot\\db_key.pem")
 
 if sys.platform == "linux":
     if ROOT_DIRECTORY in sys.path[0].split('/'):
@@ -40,14 +42,14 @@ if sys.platform == "linux":
         MODULE_DIR = ROOT_DIR
 
     HELP_FILE = os.path.join(MODULE_DIR, "Bot/help.txt")
-    try :
-      with open(HELP_FILE) as fd:
-        pass
-    except FileNotFoundError:
-      HELP_FILE = os.path.join(sys.path[0] , "src/Bot/help.txt")
     CERTIFICATE_FILE = os.path.join(MODULE_DIR, "Bot/db_key.pem")
+    try:
+        with open(HELP_FILE) as fd:
+            pass
+    except FileNotFoundError:
+        HELP_FILE = os.path.join(sys.path[0], "src/Bot/help.txt")
+        CERTIFICATE_FILE = os.path.join(MODULE_DIR, "src/Bot/db_key.pem")
 
-COMMAND_LIST = ["/help", "/weather", "/itinerary", "/resto", "/cine", "/news", "/opinion"]
 
 # Link nominatim openstreetmap
 def itinerary_link(address):
