c8526c71803414822c1a55ed251bec6dbeb63da9
CardinPatson <74672498+CardinPatson@users.noreply.github.com>
2022-01-23 14:04:11 +0100

    update test

--- test/test_bot.py
+++ test/test_bot.py
@@ -15,8 +15,7 @@ class TestBot(unittest.TestCase):
         pass
 
     def test_get_help(self):
-        # self.assertEqual(len(Bot(Request("/help"), self.true_command_list).get_help(HELP_FILE)), 913)
-        pass
+        self.assertEqual(len(Bot("/help").get_help(HELP_FILE)), 915)
 
     def test_process_request(self):
 
@@ -26,7 +25,7 @@ class TestBot(unittest.TestCase):
         self.assertEqual(len(Bot("/cine namur").process_request(
             Request("/cine namur").get_message(self.true_command_list))), 187)
         self.assertEqual(len(Bot("/news basket").process_request(
-            Request("/news basket").get_message(self.true_command_list)).split("\n")), 1)
+            Request("/news basket").get_message(self.true_command_list)).split("\n")), 10)
         self.assertEqual(len(Bot("/resto gembloux").process_request(
             Request("/resto gembloux").get_message(self.true_command_list))), 84)
 
